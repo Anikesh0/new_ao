26a73a81fe3ef5bd7ba420fff4dbf92d856a227a
Anikesh0 <singhanikesh230@gmail.com>
2025-05-23 15:23:52 +0530

    Update new_ao.py

--- new_ao.py
+++ new_ao.py
@@ -3,8 +3,12 @@ import pandas as pd
 import numpy as pd 
 from PIL import Image
 
-
-df = pd.read_excel("Adidas.xlsx")
+def load_data():
+    data = pd.read_csv("youtube_channel_data.csv")
+    data['DATE'] = pd.to_datetime(data['DATE'])
+    data['NET_SUBSCRIBERS'] = data['SUBSCRIBERS_GAINED'] - data['SUBSCRIBERS_LOST']
+    return data
+    
 print("Hello world ")
 option = st.selectbox(
     "How would you like to be contacted?",
