aa397a107f05280161a8756a7394af44d27ea78e
Anikesh0 <singhanikesh230@gmail.com>
2025-05-23 15:34:20 +0530

    Update new_ao.py

--- new_ao.py
+++ new_ao.py
@@ -18,4 +18,19 @@ def load_data():
 # Load data
 df = load_data()
 
-# Set up input widgets
+
+with st.sidebar:
+    st.title(" Anikesh Dashboard")
+    st.header("⚙️ Settings")
+    
+    max_date = df['DATE'].max().date()
+    min_date = df['DATE'].min().date()
+    default_start_date = min_date
+    default_end_date = max_date
+    start_date = st.date_input("Start date", default_start_date, min_value=df['DATE'].min().date(), max_value=max_date)
+    end_date = st.date_input("End date", default_end_date, min_value=df['DATE'].min().date(), max_value=max_date)
+    time_frame = st.selectbox("Select time frame",
+                              ("Daily", "Weekly", "Monthly", "Quarterly"),
+    )
+    chart_selection = st.selectbox("Select a chart type",
+                                   ("Bar","Pie chart","Line chart","Area"))
