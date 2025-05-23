b96e32c063b024d418904f97a232af86808d931e
Anikesh0 <singhanikesh230@gmail.com>
2025-05-23 14:58:11 +0530

    Update new_ao.py

--- new_ao.py
+++ new_ao.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as pd 
+from PIL import Image
 print("Hello world ")
+option = st.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone"),
+)
+
+st.write("You selected:", option)
