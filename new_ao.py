0f2993c06613a32c3b75b4f06978346510e7fa74
Anikesh0 <singhanikesh230@gmail.com>
2025-05-23 15:17:48 +0530

    Update new_ao.py

--- new_ao.py
+++ new_ao.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as pd 
 from PIL import Image
-import plotly.express as px
-import plotly.graph_objects as go
+
 
 df = pd.read_excel("Adidas.xlsx")
 print("Hello world ")
